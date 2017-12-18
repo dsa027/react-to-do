caa9ad6ba8d00cb7f3a924c21d5e1ef7f9a2a0e6
David Alderson <dsa027@gmail.com>
2017-12-18 16:55:11 -0700

    create component ToDo.js to show example of several todos

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
+        <h3>ToDo</h3>
         <ul>
-          <ToDo />
-          <ToDo />
+          {this.state.todos.map((todo, index) => {
+            return <ToDo
+              key={index}
+              description={todo.description}
+              isCompleted={todo.isCompleted}
+            />
+          })}
         </ul>
       </div>
     );
