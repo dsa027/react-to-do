1d8e9ee471ca484d8c01ab7e6feb4edc87a2c22b
David Alderson <dsa027@gmail.com>
2017-12-18 21:13:38 -0700

    Had extra ul tag; used css on ul to make list correctly centered.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,19 +3,17 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <ul>
-        <li>
-          <input
-            type="checkbox"
-            checked={this.props.isCompleted}
-            onChange={this.props.toggleComplete}
-          />
-          <span>{this.props.description}</span>
-          <button style={{'marginLeft': '5px'}} type='submit' onClick={this.props.deleteToDo}>
-            Delete
-          </button>
-        </li>
-      </ul>
+      <li>
+        <input
+          type="checkbox"
+          checked={this.props.isCompleted}
+          onChange={this.props.toggleComplete}
+        />
+        <span>{this.props.description}</span>
+        <button style={{'marginLeft': '5px'}} type='submit' onClick={this.props.deleteToDo}>
+          Delete
+        </button>
+      </li>
     );
   }
 }
